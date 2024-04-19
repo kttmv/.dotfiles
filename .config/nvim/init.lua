a3f7db602046679f27b38a8173728f59071b4558
Vlad <ktototamovich20@gmail.com>
2024-04-19 12:55:39 +0300

    show hidden files in telescope.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -64,6 +64,15 @@ require("lazy").setup({
         'nvim-telescope/telescope.nvim', 
         branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('telescope').setup {
+                pickers = {
+                    find_files = {
+                        hidden = true
+                    }
+                }
+            }
+        end
     },
     {
         'feline-nvim/feline.nvim',
