a39f6e49bd4fde24c214d63b18bd5754673a5304
Vlad <ktototamovich20@gmail.com>
2024-04-24 00:07:09 +0300

    updated nixpkgs to use actual version

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "nixpkgs/c03f3028384fbcd7f3562344cbf8f3583fc96369";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    #nixpkgs.url = "nixpkgs/c03f3028384fbcd7f3562344cbf8f3583fc96369";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
