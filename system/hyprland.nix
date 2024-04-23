3a6a0a58cbfbd3ac8c42162994cc05a06d6d3fed
Vlad <ktototamovich20@gmail.com>
2024-04-24 01:49:47 +0300

    -nautilus, +cinnamon.nemo

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -25,11 +25,11 @@
     hyprshot # screenshots for hyprland
     hyprlock # screen locking utility
     gnome.gnome-calculator # calculator
-    gnome.nautilus # file manager
     kitty # default terminal emulator in hyprland
     wlogout # logout menu
     wl-clip-persist # to keep Wayland clipboard after programs close
     wl-clipboard # required by cliphist
     cliphist # clipboard manager
+    cinnamon.nemo # file manager
   ];
 }
