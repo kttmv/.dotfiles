91c666a1d65e06b99482602e0964e27998a9875f
Vlad <ktototamovich20@gmail.com>
2024-07-09 14:34:39 +0300

    moved most packages to home-manager

--- apps/packages.nix
+++ apps/packages.nix
@@ -6,35 +6,5 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    obsidian # notes on steroids
-    okular # pdf reader
-    onlyoffice-bin # office alternative
-    pinta # paint alternative
-    gh # github
-    helix # vim killer (?)
-    lazygit # nice terminal ui for git
-    microsoft-edge-dev # an evil browser
-    vscode # an evil code editor
-    webcord # electron wrapper for web version of discrod
-    zellij # a tmux killer
-    nh # nix cli helper 
-    firefox # not so evil browser
-    csharpier # an opinionated code formatter for C#
-
-    (with dotnetCorePackages; combinePackages [
-      sdk_6_0
-      sdk_7_0
-      sdk_8_0
-    ])
-
-    dotnetPackages.Nuget
-    gcc
-    git
-    nodePackages_latest.nodejs
-    nodePackages_latest."@nestjs/cli"
-    python3
-    rustup
-    wget
-    wineWowPackages.waylandFull
   ];
 }
--- home/packages.nix
+++ home/packages.nix
@@ -1,7 +1,51 @@
 { pkgs, mongodb-pkgs, ... }:
 
 let
-  packagesList = with pkgs; [ lf pavucontrol ];
+  packagesList = with pkgs; [
+    lf # cli file browser
+    zellij # tmux killer
+    nh # nix cli helper 
+
+    pavucontrol # audio control panel
+
+    obsidian # notes on steroids
+
+    okular # pdf reader
+    onlyoffice-bin # office alternative
+    pinta # paint alternative
+
+    webcord # electron wrapper for web version of discrod
+
+    firefox # not so evil browser
+    microsoft-edge-dev # an evil browser
+
+    #############################
+    # DEV
+    #############################
+
+    helix # vim killer (?)
+    vscode # an evil code editor
+
+    git
+    lazygit # nice terminal ui for git
+    gh # github
+
+    # different dotnet core versions
+    (with dotnetCorePackages; combinePackages [
+      sdk_6_0
+      sdk_7_0
+      sdk_8_0
+    ])
+    dotnetPackages.Nuget
+    csharpier # an opinionated code formatter for C#
+
+    gcc
+    nodePackages_latest.nodejs
+    nodePackages_latest."@nestjs/cli"
+    python3
+    rustup
+    wget
+  ];
   packagesListMongoDB = with mongodb-pkgs; [ mongodb ];
 in
 {
