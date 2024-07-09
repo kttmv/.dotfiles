db4fe2d523ed4bc22516494156ff18742ea45efa
Vlad <ktototamovich20@gmail.com>
2024-07-09 14:50:42 +0300

    moved packages to home-manager

--- home/packages.nix
+++ home/packages.nix
@@ -2,9 +2,12 @@
 
 let
   packagesList = with pkgs; [
-    lf # cli file browser
+    # CLI
+    lf # file browser
     zellij # tmux killer
     nh # nix cli helper 
+    ripgrep # regex search tool, used in telescope
+    wget # tool for downloading files
 
     pavucontrol # audio control panel
 
@@ -16,38 +19,41 @@ let
 
     webcord # electron wrapper for web version of discrod
 
+    # BROWSERS
     firefox # not so evil browser
     microsoft-edge-dev # an evil browser
-
-    #############################
-    # DEV
-    #############################
-
+  ];
+  packagesListMongoDB = with mongodb-pkgs; [ mongodb ];
+  packagesListDev = with pkgs; [
+    # EDITORS
     helix # vim killer (?)
     vscode # an evil code editor
 
+    # GIT
     git
     lazygit # nice terminal ui for git
     gh # github
 
-    # different dotnet core versions
+    # DOTNET
     (with dotnetCorePackages; combinePackages [
       sdk_6_0
       sdk_7_0
       sdk_8_0
     ])
     dotnetPackages.Nuget
-    csharpier # an opinionated code formatter for C#
 
+    # LANGUAGES
     gcc
     nodePackages_latest.nodejs
     nodePackages_latest."@nestjs/cli"
     python3
     rustup
-    wget
+
+    # FORMATTERS
+    csharpier # an opinionated code formatter for C#
+    nixpkgs-fmt # nix language formatter
   ];
-  packagesListMongoDB = with mongodb-pkgs; [ mongodb ];
 in
 {
-  home.packages = packagesList ++ packagesListMongoDB;
+  home.packages = packagesList ++ packagesListMongoDB ++ packagesListDev;
 }
